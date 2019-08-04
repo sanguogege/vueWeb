76c0908ce0bd64aca4b6ce1ba737b7867b8ebbef
zeng yu <981952053@qq.com>
2019-08-05 00:26:33 +0800

    111111

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="nav">
+      <router-link to="/">摄影机不要停</router-link> |
+      <router-link to="/about">xx和他的爷爷</router-link>
     </div>
     <router-view/>
   </div>
@@ -13,10 +13,10 @@
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
+  text-align: center;
 }
-#nav {
+.nav {
   padding: 30px;
   a {
     font-weight: bold;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1 v-on:click="ss">This is an about page</h1>
+    <p>{{axero.a}}</p>
+    <p>{{axero.b}}</p>
     <HelloWorld msg="hahha"/>
   </div>
 </template>
@@ -13,6 +15,25 @@ export default {
   name: 'home',
   components: {
     HelloWorld
+  },
+  data: function(){
+    return {
+            axero: {
+              a:'这是data返回的数据111111',
+              b:'这是data返回的数据22222',
+            },
+          }
+  },
+  methods: {
+    ss: function(){
+        alert("11")
+    }
   }
 }
 </script>
+
+<style>
+  .about h1 {
+    color: red;
+  }
+</style>
